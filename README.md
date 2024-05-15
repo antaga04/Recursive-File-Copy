ff4dbac42ee4b06dac86cbb984d74339ecb06785
antaga04 <antaga04@gmail.com>
2024-05-15 14:06:43 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Project 1 - Operating Systems
+
+## Option 2
+
+Recursive file copy program [24p]
+The program takes two arguments: a source directory and a destination directory. The destination directory cannot exist. The program copies all the files from the source directory to the destination directory. If a source directory contains a directory, this directory is copied to the destination together with the contents. This procedure is applied recursively.
+
+The SIGINT signal (Ctrl + C) should stop the currently performed copy operation and delete the incomplete destination file. This situation should be logged in the system log (syslog).
+
+[10p] Additional option -a preserves the attributes of files. That is all the attributes of files in a destination directory (owner, group, access mode, three access times) are the same as the attributes of corresponding files in the source directory.
+
+## Team
+
+- Jorge Ortega Izquierdo
+- Adrián Anta Gil
+
+## How to run program
+
+- Paso 1
+
+  ```bash
+  gcc -o recursive_copy recursive_copy.c
+  ```
+
+- Paso 2
+
+  Note that if `./test_dir/destination` already exists you shoukd either delete it or change the destination directory.
+
+  ```bash
+  ./recursive_copy ./test_dir/source ./test_dir/destination
+  ```
+
+## Explanation
+
+The program takes 2 arguments, a source directory (to copy) and a destination directory (that doesn't exit), the source will be copied in the destination directory and each directory in the source will be copied using the same function recursively.
---
+++ recursive_copy.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <syslog.h>
+#include <limits.h>
+
+#define BUFFER_SIZE 1024
+
+int preserve_attributes = 0;
+
+void handle_sigint(int sig)
+{
+  syslog(LOG_INFO, "Copying interrupted. Deleting incomplete destination file...");
+  closelog();
+  exit(EXIT_FAILURE);
+}
+
+void copy_file(const char *source, const char *destination)
+{
+  int source_fd, dest_fd;
+  ssize_t bytes_read, bytes_written;
+  char buffer[BUFFER_SIZE];
+
+  source_fd = open(source, O_RDONLY);
+  if (source_fd == -1)
+  {
+    perror("Error opening source file");
+    exit(EXIT_FAILURE);
+  }
+
+  dest_fd = open(destination, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+  if (dest_fd == -1)
+  {
+    perror("Error opening destination file");
+    exit(EXIT_FAILURE);
+  }
+
+  while ((bytes_read = read(source_fd, buffer, BUFFER_SIZE)) > 0)
+  {
+    bytes_written = write(dest_fd, buffer, bytes_read);
+    if (bytes_written != bytes_read)
+    {
+      perror("Error writing to destination file");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  if (bytes_read == -1)
+  {
+    perror("Error reading from source file");
+    exit(EXIT_FAILURE);
+  }
+
+  if (preserve_attributes)
+  {
+    struct stat st;
+    if (stat(source, &st) == -1)
+    {
+      perror("Error getting file attributes");
+      exit(EXIT_FAILURE);
+    }
+    if (fchmod(dest_fd, st.st_mode) == -1)
+    {
+      perror("Error setting file mode");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  if (close(source_fd) == -1 || close(dest_fd) == -1)
+  {
+    perror("Error closing files");
+    exit(EXIT_FAILURE);
+  }
+}
+
+void copy_directory(const char *source, const char *destination)
+{
+  DIR *dir;
+  struct dirent *entry;
+  struct stat st;
+  char source_path[PATH_MAX], dest_path[PATH_MAX];
+
+  dir = opendir(source);
+  if (dir == NULL)
+  {
+    perror("Error opening source directory");
+    exit(EXIT_FAILURE);
+  }
+
+  mkdir(destination, 0777);
+
+  while ((entry = readdir(dir)) != NULL)
+  {
+    if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+    {
+      continue;
+    }
+
+    snprintf(source_path, PATH_MAX, "%s/%s", source, entry->d_name);
+    snprintf(dest_path, PATH_MAX, "%s/%s", destination, entry->d_name);
+
+    if (lstat(source_path, &st) == -1)
+    {
+      perror("Error getting file status");
+      exit(EXIT_FAILURE);
+    }
+
+    if (S_ISDIR(st.st_mode))
+    {
+      copy_directory(source_path, dest_path);
+    }
+    else
+    {
+      copy_file(source_path, dest_path);
+    }
+  }
+
+  if (closedir(dir) == -1)
+  {
+    perror("Error closing source directory");
+    exit(EXIT_FAILURE);
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc < 3)
+  {
+    fprintf(stderr, "Usage: %s source_directory destination_directory [-a]\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  if (argc == 4 && strcmp(argv[3], "-a") == 0)
+  {
+    preserve_attributes = 1;
+  }
+
+  if (signal(SIGINT, handle_sigint) == SIG_ERR)
+  {
+    perror("Error setting signal handler");
+    exit(EXIT_FAILURE);
+  }
+
+  openlog("RecursiveFileCopy", LOG_PID | LOG_CONS, LOG_USER);
+
+  const char *source = argv[1];
+  const char *destination = argv[2];
+
+  if (mkdir(destination, 0777) == -1 && errno != EEXIST)
+  {
+    perror("Error creating destination directory");
+    exit(EXIT_FAILURE);
+  }
+
+  copy_directory(source, destination);
+
+  syslog(LOG_INFO, "Copying completed successfully");
+  closelog();
+  return 0;
+}
